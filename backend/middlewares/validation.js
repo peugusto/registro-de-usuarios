a43276672c949e0e86177eb17420ffda00fca5b5
Pedro Augusto <157862699+peugusto@users.noreply.github.com>
2025-08-29 21:27:52 -0300

    Update validation.js

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -1,7 +1,9 @@
 export default function validate(req,res,next){
     const { firstname, lastname} = req.body;
     
-    if(!firstname?.trim() || !lastname?.trim()){
+    if( typeof firstname !== 'string' || firstname.trim() === '' ||  
+        typeof lastname !== 'string' || lastname.trim() === ''){
+        
         console.error('Todos os campos precisam estar preenchidos.')
         return res.status(400).json({ error: 'Todos os campos precisam estar preenchidos.' });
     }
